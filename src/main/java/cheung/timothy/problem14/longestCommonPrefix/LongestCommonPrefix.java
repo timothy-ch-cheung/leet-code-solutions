6abd9369dc6697a74296a7a4bb1e452b9f3e995c
Tim Cheung <43986931+timothy-ch-cheung@users.noreply.github.com>
2022-12-10 00:34:40 +0000

    Add solution to Problem 14

--- src/main/java/cheung/timothy/problem14/longestCommonPrefix/LongestCommonPrefix.java
+++ src/main/java/cheung/timothy/problem14/longestCommonPrefix/LongestCommonPrefix.java
@@ -2,6 +2,29 @@ package cheung.timothy.problem14.longestCommonPrefix;
 
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strings) {
-        return "";
+        boolean mismatched = false;
+        int i = 0;
+        StringBuilder prefix = new StringBuilder();
+
+        while (!mismatched) {
+            Character currentChar = null;
+            for (String s : strings) {
+                if (i >= s.length()) {
+                    return prefix.toString();
+                }
+                if (currentChar == null) {
+                    currentChar = s.charAt(i);
+                }
+                if (s.charAt(i) != currentChar) {
+                    mismatched = true;
+                    break;
+                }
+            }
+            if (!mismatched) {
+                prefix.append(currentChar);
+                i++;
+            }
+        }
+        return prefix.toString();
     }
 }

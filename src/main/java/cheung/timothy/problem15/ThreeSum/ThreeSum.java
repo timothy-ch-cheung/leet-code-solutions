ec88e7f3f7ee87835cd30d708bea9eda35df00fa
Tim Cheung <43986931+timothy-ch-cheung@users.noreply.github.com>
2022-12-12 19:33:21 +0000

    Add tests for problem 15: Three Sum

---
+++ src/main/java/cheung/timothy/problem15/ThreeSum/ThreeSum.java
@@ -0,0 +1,9 @@
+package cheung.timothy.problem15.ThreeSum;
+
+import java.util.List;
+
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        return null;
+    }
+}
---
+++ src/test/java/cheung/timothy/problem15/ThreeSum/ThreeSumTest.java
@@ -0,0 +1,41 @@
+package cheung.timothy.problem15.ThreeSum;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class ThreeSumTest {
+
+    private static final ThreeSum solution = new ThreeSum();
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void reverse(int[] input, List<List<Integer>> expected) {
+        List<List<Integer>> answer = solution.threeSum(input);
+        assertNotNull(answer);
+        assertThat(answer.size(), is(expected.size()));
+        for (List<Integer> triplet : expected) {
+            assertThat(answer, hasItem(triplet));
+        }
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of(new int[]{-1, 0, 1, 2, -1, -4}, Arrays.asList(Arrays.asList(-1, -1, 2), Arrays.asList(-1, 0, 1))),
+                Arguments.of(new int[]{0, 1, 1}, new ArrayList<Integer>()),
+                Arguments.of(new int[]{0, 0, 0}, Collections.singletonList(Arrays.asList(0, 0, 0)))
+        );
+    }
+
+}

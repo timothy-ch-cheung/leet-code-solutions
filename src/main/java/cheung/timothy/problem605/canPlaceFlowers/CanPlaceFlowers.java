cbe06f13fb89d33c24adab39a541f4777bba3855
Tim Cheung <43986931+timothy-ch-cheung@users.noreply.github.com>
2024-11-23 02:38:08 +0000

    Add solution to problem 605

---
+++ src/main/java/cheung/timothy/problem605/canPlaceFlowers/CanPlaceFlowers.java
@@ -0,0 +1,29 @@
+package cheung.timothy.problem605.canPlaceFlowers;
+
+public class CanPlaceFlowers {
+
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int spaces = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (isFree(flowerbed, i)) {
+                spaces++;
+                flowerbed[i] = 1;
+            }
+        }
+        return n <= spaces;
+    }
+
+    private boolean isFree(int[] flowerbed, int idx) {
+        int left = idx - 1;
+        int right = idx + 1;
+        if (left < 0 && right > flowerbed.length - 1) {
+            return flowerbed[idx] == 0;
+        } else if (left < 0) {
+            return flowerbed[idx] == 0 && flowerbed[right] == 0;
+        } else if (right > flowerbed.length - 1) {
+            return flowerbed[idx] == 0 && flowerbed[left] == 0;
+        } else {
+            return flowerbed[idx] == 0 && flowerbed[left] == 0 && flowerbed[right] == 0;
+        }
+    }
+}
---
+++ src/test/java/cheung/timothy/problem605/canPlaceFlowers/CanPlaceFlowersTest.java
@@ -0,0 +1,31 @@
+package cheung.timothy.problem605.canPlaceFlowers;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class CanPlaceFlowersTest {
+
+    private static final CanPlaceFlowers solution = new CanPlaceFlowers();
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void greatestNumberOfCandies(int[] flowerbed, int n, boolean canPlaceFlowers) {
+        boolean answer = solution.canPlaceFlowers(flowerbed, n);
+        assertThat(answer, is(canPlaceFlowers));
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 0, 0, 0, 1}, 1, true),
+                Arguments.of(new int[]{1, 0, 0, 0, 1}, 2, false),
+                Arguments.of(new int[]{1, 0, 0, 0, 1, 0, 0}, 2, true),
+                Arguments.of(new int[]{0}, 1, true)
+        );
+    }
+}

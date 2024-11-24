a68724e59d024b29eb83afc5cccd4474f98bbc69
Tim Cheung <43986931+timothy-ch-cheung@users.noreply.github.com>
2024-11-24 01:30:28 +0000

    use for efficient solution for problem 334

--- src/main/java/cheung/timothy/problem0334/increasingTripletSubsequence/IncreasingTripletSubsequence.java
+++ src/main/java/cheung/timothy/problem0334/increasingTripletSubsequence/IncreasingTripletSubsequence.java
@@ -6,13 +6,15 @@ public class IncreasingTripletSubsequence {
         if (nums.length < 3) {
             return false;
         }
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
-                for (int k = j+1; k < nums.length; k++) {
-                    if (nums[i] < nums[j] && nums[j] < nums[k]) {
-                        return true;
-                    }
-                }
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+        for (int num: nums) {
+            if (num < first) {
+                first = num;
+            } else if (num < second && num > first) {
+                second = num;
+            } else if (first < second && second < num) {
+                return true;
             }
         }
         return false;
--- src/test/java/cheung/timothy/problem0334/increasingTripletSubsequence/IncreasingTripletSubsequenceTest.java
+++ src/test/java/cheung/timothy/problem0334/increasingTripletSubsequence/IncreasingTripletSubsequenceTest.java
@@ -24,7 +24,8 @@ class IncreasingTripletSubsequenceTest {
         return Stream.of(
                 Arguments.of(new int[]{1, 2, 3, 4, 5}, true),
                 Arguments.of(new int[]{5, 4, 3, 2, 1}, false),
-                Arguments.of(new int[]{2, 1, 5, 0, 4, 6}, true)
+                Arguments.of(new int[]{2, 1, 5, 0, 4, 6}, true),
+                Arguments.of(new int[]{1, 2, 1, 3}, true)
         );
     }
 }

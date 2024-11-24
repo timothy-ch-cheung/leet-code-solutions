407dda1b909fa4330aaed73cb574426e34632464
Tim Cheung <43986931+timothy-ch-cheung@users.noreply.github.com>
2024-11-24 00:31:53 +0000

    Use more efficient solution for problem0238

--- src/main/java/cheung/timothy/problem0238/productOfArrayExceptSelf/ProductOfArrayExceptSelf.java
+++ src/main/java/cheung/timothy/problem0238/productOfArrayExceptSelf/ProductOfArrayExceptSelf.java
@@ -3,24 +3,18 @@ package cheung.timothy.problem0238.productOfArrayExceptSelf;
 public class ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
         int[] result = new int[nums.length];
-        int[] prefixProducts = new int[nums.length];
-        int[] suffixProducts = new int[nums.length];
+        result[0] = 1;
 
-        prefixProducts[0] = nums[0];
-        for (int i = 1; i < nums.length -1; i++) {
-            prefixProducts[i] = prefixProducts[i-1] * nums[i];
+        int currentProduct = 1;
+        for (int i = 1; i < nums.length; i++) {
+            currentProduct = currentProduct * nums[i-1];
+            result[i] = currentProduct;
         }
-        suffixProducts[nums.length -1] = nums[nums.length -1];
-        for (int i = nums.length -2; i >= 0; i--) {
-            suffixProducts[i] = suffixProducts[i+1] * nums[i];
+        currentProduct = 1;
+        for (int i = nums.length-2; i >= 0; i--) {
+            currentProduct = currentProduct * nums[i+1];
+            result[i] = currentProduct * result[i];
         }
-
-        result[0] = suffixProducts[1];
-        result[nums.length-1] = prefixProducts[nums.length-2];
-        for (int i = 1; i < nums.length-1; i++) {
-            result[i] = prefixProducts[i-1] * suffixProducts[i+1];
-        }
-
         return result;
     }
 }
